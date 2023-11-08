39a4c3cc669bcf8d2c28422344b089ae0f5ec49b
Aashish <aashishadhikari693@gmail.com>
2023-11-08 13:22:13 +0545

    Made responsive navbar

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainPage from './component/MainPage';
+import Navbar from './component/NavBar/NavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Futsal Booking App</h1>
+      <p>Find and book futsal courts in your area.</p>
+      {/* Add more content and components here */}
+    </div>
+  );
+}
+
+export default MainPage;
