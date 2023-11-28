2cdc6f24d95983d3e45d3a8ae2303491db36b934
Aashish <aashishadhikari693@gmail.com>
2023-11-28 20:00:06 +0545

    Implemented User page

--- client/src/component/User/User.js
+++ client/src/component/User/User.js
@@ -1,65 +1,44 @@
 // UserPage.js
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import './User.css';
+import {Futsals} from './Futsal.js';
 
 const UserPage = () => {
-  const [futsals, setFutsals] = useState([]);
-  const [sortBy, setSortBy] = useState('location');
-  const [showOptions, setShowOptions] = useState(false);
-
-  const handleSortChange = (event) => {
-    setSortBy(event.target.value);
-  };
+const [showOptions, setShowOptions] = useState(false);
 
   const handleAvatarClick = () => {
     setShowOptions(!showOptions);
   };
 
-  useEffect(() => {
-    // Fetch futsals from the JSON file
-    fetch('/path/to/futsals.json')
-      .then((response) => response.json())
-      .then((data) => setFutsals(data))
-      .catch((error) => console.error('Error fetching futsals:', error));
-  }, []);
-
-  const sortedFutsals = [...futsals].sort((a, b) =>
-    a[sortBy].localeCompare(b[sortBy])
-  );
 
   return (
     <div className="user-page-container">
       <div className="avatar-container" onClick={handleAvatarClick}>
-        {/* Display user avatar here */}
+        <img src='/image.jpeg' alt='Avatar'/>
         {/* You can add an image or an icon as per your design */}
       </div>
       {showOptions && (
         <div className="profile-options">
-          <p>Profile Options:</p>
+          <p>User A</p>
           <ul>
             <li>Settings</li>
             <li>Sign Out</li>
-            {/* Add more options as needed */}
           </ul>
         </div>
       )}
-      <h1>User Page</h1>
-      <label>
-        Sort by:
-        <select value={sortBy} onChange={handleSortChange}>
-          <option value="location">Location</option>
-          {/* Add more options for other sorting criteria if needed */}
-        </select>
-      </label>
+      <h1>Welcome to Book-Futsal, Select the futsal Grounds</h1>
       <div className="futsal-list-container">
-        <h2>List of Registered Futsal Grounds</h2>
-        <ul>
-          {sortedFutsals.map((futsal) => (
-            <li key={futsal.id}>
-              {futsal.name} - {futsal.location}
-            </li>
-          ))}
-        </ul>
+        <br />
+      <h2>---->List of Registered Futsal Grounds:</h2>
+     
+      <ul>
+        {Futsals.map((futsal, index) => (
+        <li key={index}>
+        <p>{index+1}.&nbsp;  &nbsp; Name: {futsal.name} &nbsp;  &nbsp; Location: {futsal.location}</p>
+        </li>
+        ))}
+      </ul>
+
       </div>
     </div>
   );
